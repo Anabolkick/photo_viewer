ced04a0e049d02bc94417b3345983dcf3696fcd2
Anabolkick <oleg10galysh@gmail.com>
2021-11-30 21:50:40 +0200

    Add project files.

---
+++ lab7_2/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿
+namespace lab7_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.open_file_btn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.open_folder_btn = new System.Windows.Forms.Button();
+            this.prev_btn = new System.Windows.Forms.Button();
+            this.next_btn = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.selected_ph_label = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // open_file_btn
+            // 
+            this.open_file_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.open_file_btn.Location = new System.Drawing.Point(21, 350);
+            this.open_file_btn.Name = "open_file_btn";
+            this.open_file_btn.Size = new System.Drawing.Size(195, 44);
+            this.open_file_btn.TabIndex = 0;
+            this.open_file_btn.Text = "Відкрити окреме фото";
+            this.open_file_btn.UseVisualStyleBackColor = true;
+            this.open_file_btn.Click += new System.EventHandler(this.open_file_btn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(756, 596);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.treeView1.Location = new System.Drawing.Point(21, 46);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(195, 277);
+            this.treeView1.TabIndex = 2;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // open_folder_btn
+            // 
+            this.open_folder_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.open_folder_btn.Location = new System.Drawing.Point(21, 400);
+            this.open_folder_btn.Name = "open_folder_btn";
+            this.open_folder_btn.Size = new System.Drawing.Size(195, 44);
+            this.open_folder_btn.TabIndex = 3;
+            this.open_folder_btn.Text = "Відкрити папку з фото";
+            this.open_folder_btn.UseVisualStyleBackColor = true;
+            this.open_folder_btn.Click += new System.EventHandler(this.open_folder_btn_Click);
+            // 
+            // prev_btn
+            // 
+            this.prev_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.prev_btn.Location = new System.Drawing.Point(21, 525);
+            this.prev_btn.Name = "prev_btn";
+            this.prev_btn.Size = new System.Drawing.Size(96, 47);
+            this.prev_btn.TabIndex = 4;
+            this.prev_btn.Text = "Минуле";
+            this.prev_btn.UseVisualStyleBackColor = true;
+            this.prev_btn.Click += new System.EventHandler(this.prev_btn_Click);
+            // 
+            // next_btn
+            // 
+            this.next_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.next_btn.Location = new System.Drawing.Point(120, 525);
+            this.next_btn.Name = "next_btn";
+            this.next_btn.Size = new System.Drawing.Size(96, 47);
+            this.next_btn.TabIndex = 5;
+            this.next_btn.Text = "Наступне";
+            this.next_btn.UseVisualStyleBackColor = true;
+            this.next_btn.Click += new System.EventHandler(this.next_btn_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.selected_ph_label);
+            this.splitContainer1.Panel1.Controls.Add(this.open_file_btn);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+            this.splitContainer1.Panel1.Controls.Add(this.open_folder_btn);
+            this.splitContainer1.Panel1.Controls.Add(this.next_btn);
+            this.splitContainer1.Panel1.Controls.Add(this.prev_btn);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(1003, 600);
+            this.splitContainer1.SplitterDistance = 239;
+            this.splitContainer1.TabIndex = 6;
+            // 
+            // selected_ph_label
+            // 
+            this.selected_ph_label.AutoSize = true;
+            this.selected_ph_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.selected_ph_label.Location = new System.Drawing.Point(23, 466);
+            this.selected_ph_label.Name = "selected_ph_label";
+            this.selected_ph_label.Size = new System.Drawing.Size(116, 20);
+            this.selected_ph_label.TabIndex = 7;
+            this.selected_ph_label.Text = "Обране фото:\r\n";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(23, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Фото в обраних папках";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1003, 600);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Image Viewer";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button open_file_btn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button open_folder_btn;
+        private System.Windows.Forms.Button prev_btn;
+        private System.Windows.Forms.Button next_btn;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label selected_ph_label;
+    }
+}
+
---
+++ lab7_2/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace lab7_2
+{
+    public partial class Form1 : Form
+    {
+        private int nodeIndex;
+        public Form1()
+        {
+            InitializeComponent();
+            nodeIndex = 0;
+        }
+
+
+        private void open_file_btn_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Image(*.PNG;*.JPG;*.JPEG;)|*.PNG;*.JPG*.JPEG;|All files (*.*)|*.*";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                var filePath = openFileDialog1.FileName;
+                var name = filePath.Substring(filePath.LastIndexOf('\\') + 1);
+                pictureBox1.Image = Image.FromFile(filePath);
+                selected_ph_label.Text = $"Обране фото: {Environment.NewLine} {name}";
+            }
+        }
+
+        private void open_folder_btn_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                var files = Directory.GetFiles(folderBrowserDialog1.SelectedPath);
+                var dir_name = folderBrowserDialog1.SelectedPath.Substring(folderBrowserDialog1.SelectedPath.LastIndexOf('\\') + 1);
+                treeView1.Nodes.Add(dir_name, dir_name);
+                foreach (string file in files)
+                {
+                    if (file.EndsWith(".jpeg") || file.EndsWith(".jpg") || file.EndsWith(".png"))
+                    {
+                        var name = file.Substring(file.LastIndexOf('\\') + 1);
+                        treeView1.Nodes[nodeIndex].Nodes.Add(file, name);
+                    }
+                }
+                if (treeView1.Nodes[nodeIndex].Nodes[0] != null)
+                {
+                    treeView1.SelectedNode = treeView1.Nodes[nodeIndex].Nodes[0];
+                    pictureBox1.Image = Image.FromFile(treeView1.SelectedNode.Name);
+                }
+                nodeIndex++;
+            }
+        }
+
+        private void prev_btn_Click(object sender, EventArgs e)
+        {
+            if (treeView1.SelectedNode.PrevNode != null)
+            {
+                treeView1.SelectedNode = treeView1.SelectedNode.PrevNode;
+                pictureBox1.Image = Image.FromFile(treeView1.SelectedNode.Name);
+            }
+        }
+
+        private void next_btn_Click(object sender, EventArgs e)
+        {
+            if (treeView1.SelectedNode.NextNode != null)
+            {
+                treeView1.SelectedNode = treeView1.SelectedNode.NextNode;
+                pictureBox1.Image = Image.FromFile(treeView1.SelectedNode.Name);
+            }
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            try
+            {
+                pictureBox1.Image = Image.FromFile(treeView1.SelectedNode.Name);
+                selected_ph_label.Text = $"Обране фото: {Environment.NewLine} {treeView1.SelectedNode.Text}";
+            }
+            catch
+            {
+                if (treeView1.SelectedNode.Nodes[0] != null)
+                {
+                    pictureBox1.Image = Image.FromFile(treeView1.SelectedNode.Nodes[0].Name);
+                    selected_ph_label.Text = $"Обране фото: {Environment.NewLine} {treeView1.SelectedNode.Nodes[0].Text}";
+                }
+            }
+        }
+
+    }
+}
